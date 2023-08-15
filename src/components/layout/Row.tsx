775174995df99e1af00282e801d0b3188d592284
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-15 22:36:59 +0300

    add a center property since we use it a lot

--- src/components/layout/Row.tsx
+++ src/components/layout/Row.tsx
@@ -1,10 +1,11 @@
-import { ComponentProps, splitProps } from "solid-js";
+import {ComponentProps, splitProps} from "solid-js";
 
 export interface RowProps extends ComponentProps<"div"> {
     class?: string;
+    center?: boolean;
 }
 
 export default function Row(props: RowProps) {
     const [local, div] = splitProps(props, ["class"]);
-    return <div class={`flex flex-row items-center ${local.class || ""}`} {...div} />;
+    return <div class={`flex flex-row items-center ${props.center ? "center" : ""} ${local.class || ""}`} {...div} />;
 }
