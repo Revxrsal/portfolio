3f0fdc81945d96eba699a9a409d694436d5ce4f7
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-15 22:35:29 +0300

    add text-[size] classes to safelist

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+        extend: {}
+    },
+    safelist: [
+        {
+            pattern: /text-(base|xl|2xl|3xl|4xl|5xl|6xl)/,
+        },
+    ],
+    plugins: []
 };
