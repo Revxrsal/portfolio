1c569179745a98bb36c41b8f3eb76b665bd6513d
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-16 12:59:13 +0300

    add favicon

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,6 @@
 // @refresh reload
- import {Suspense} from "solid-js";
-import {Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title,} from "solid-start";
+import {Suspense} from "solid-js";
+import {Body, ErrorBoundary, FileRoutes, Head, Html, Link, Meta, Routes, Scripts, Title,} from "solid-start";
 import "./root.css";
 
 export default function Root() {
@@ -10,6 +10,11 @@ export default function Root() {
                 <Title>Ali</Title>
                 <Meta charset="utf-8"/>
                 <Meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+                <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <Link rel="manifest" href="/site.webmanifest"/>
             </Head>
             <Body>
                 <Suspense>
