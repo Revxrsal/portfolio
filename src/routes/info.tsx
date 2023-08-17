313b885460ac685df5d5a361fb2c4f10fce50299
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2023-08-17 14:10:45 +0300

    try separating it into 2 pages?

---
+++ src/routes/info.tsx
@@ -0,0 +1,34 @@
+import {Frameworks} from "~/data/DisplayItems";
+import Divider from "~/components/decoration/Divider";
+import Header from "~/components/typography/Header";
+import {For, JSX} from "solid-js";
+import Row from "~/components/layout/Row";
+
+function LanguageIcon(props: {
+    children: JSX.Element;
+}) {
+    return <div class={`p-4 m-3 rounded bg-stone-950 fill ring-stone-200 hover:brightness-150
+     hover:scale-105 hover:ring-2 hover:ring-emerald-800 transition-all`}>
+        {props.children}
+    </div>
+}
+
+export default function Info() {
+    return <For each={Object.entries(Frameworks)}>{([name, items]) =>
+        <>
+            <Divider class={"mx-10 my-6"}/>
+            <Header class="text-center" size={5}>
+                <code>{name}</code>
+            </Header>
+            <For each={items}>{item =>
+                <Row class={"justify-evenly"}>
+                    <For each={item}>{language =>
+                        <LanguageIcon>
+                            {language.icon()}
+                        </LanguageIcon>
+                    }</For>
+                </Row>
+            }</For>
+        </>
+    }</For>;
+}
