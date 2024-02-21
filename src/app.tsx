b64e27b5b6ef0eafee3b9b8c65645860ca625c8b
Revxrsal <aloosh.kasasbeh@gmail.com>
2024-02-21 18:48:11 +0300

    add vercel analytics

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 // @refresh reload
 import {Router} from "@solidjs/router";
 import {FileRoutes} from "@solidjs/start";
-import {Suspense} from "solid-js";
+import {onMount, Suspense} from "solid-js";
 import "./app.css";
 import TopWave from "~/components/wave/TopWave";
 import BottomWave from "~/components/wave/BottomWave";
+import {inject} from "@vercel/analytics";
 
 export default function App() {
+  onMount(inject)
   return (
     <Router
       root={props => (
